e6736522b8d33b53a286ae9167b1ff4213a23ec6
kg12581 <kg12581@users.noreply.github.com>
2025-06-22 17:56:58 +0530

    Simple Chatbot Created

---
+++ chatbot.py
@@ -0,0 +1,49 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+
+from langchain.chat_models import init_chat_model
+from langchain_tavily import TavilySearch
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+tool = TavilySearch(max_results=2)
+tools = [tool]
+llm_with_fallbacks = (
+    init_chat_model("google_genai:gemini-2.0-flash").
+    with_fallbacks([
+        init_chat_model("groq:llama-3.3-70b-versatile")
+    ])
+)
+llm_with_tools = llm_with_fallbacks.bind_tools(tools)
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+graph = graph_builder.compile()
+
+user_query = input("Enter your query: ")
+graph.invoke({"messages": user_query})
